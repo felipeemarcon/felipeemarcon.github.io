901763963f3df3dd18a15bd71b94b29fd3f34c79
Felipe Marcon <felipeemarcon@gmail.com>
2021-08-10 16:59:48 -0300

    update Paragraph component to Text component

--- components/Text.js
+++ components/Text.js
@@ -1,33 +1,39 @@
 import classNames from "classnames";
 import PropTypes from "prop-types";
-import { Fragment } from "react";
 
 // Styles
-import styles from "../styles/components/Paragraph.module.scss";
+import styles from "../styles/components/Text.module.scss";
 
 const DEFAULT_PARAGRAPH_TEXT =
   "EricMauris laoreet mattis lobortis. Morbi sit amet vulputate ante, id facilisis ligula. Suspendisse lacinia bibendum dolor, vel dictum ipsum varius sit amet. Aliquam elementum fringilla cursus. Phasellus commodo quis ligula vel fermentum. Mauris luctus, arcu a volutpat ultricies, magna odio aliquam ex, eu vestibulum diam turpis ac nulla. Fusce fringilla tincidunt eros eget fermentum. Quisque eget congue lectus. Maecenas posuere sit amet erat sit amet posuere.";
 
-export default function Paragraph({
+export default function Text({
   children = DEFAULT_PARAGRAPH_TEXT,
+  tag = "p",
   size = "md",
+  weight = "500",
   theme = "onLight",
   customClass,
 }) {
-  const classes = classNames([styles.paragraph], {
+  const classes = classNames([styles.text], {
     [`${styles.sm}`]: size === "sm",
     [`${styles.md}`]: size === "md",
     [`${styles.lg}`]: size === "lg",
     [`${styles.xl}`]: size === "xl",
+    [`${styles.regular}`]: weight === "500",
     [`${styles.onLight}`]: theme === "onLight",
     [`${styles.onDark}`]: theme === "onDark",
     [`${styles.customClass}`]: customClass,
   });
 
-  return <p className={classes}>{children}</p>;
+  const Tag = tag;
+
+  return <Tag className={classes}>{children}</Tag>;
 }
 
-Paragraph.propTypes = {
-  children: PropTypes.string.isRequired,
+Text.propTypes = {
   theme: PropTypes.string,
+  tag: PropTypes.string,
+  size: PropTypes.string,
+  customClass: PropTypes.string,
 };
