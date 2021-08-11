4265ea9e2a5e530e5184260ea6dfdeb429ad3565
Felipe Marcon <felipeemarcon@gmail.com>
2021-08-11 16:24:26 -0300

    add netlify cms

---
+++ cms/config.js
@@ -0,0 +1,55 @@
+const config = {
+  cms_manual_init: true,
+  backend: {
+    name: "github",
+    repo: "felipeemarcon/felipeemarcon.github.io",
+    branch: "homolog",
+  },
+  media_folder: "public/img",
+  public_folder: "img",
+  collections: [
+    {
+      name: "globalConfigs",
+      label: "Global Configs",
+      files: [
+        {
+          label: "Configs",
+          name: "configs",
+          file: "content/global/configs.md",
+          fields: [
+            {
+              label: "Contact Email",
+              name: "contact_email",
+              widget: "string",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "homeSections",
+      label: "Home Sections",
+      files: [
+        {
+          label: "Hero Section",
+          name: "hero",
+          file: "content/home/sections/hero.md",
+          fields: [
+            {
+              label: "Hero Title",
+              name: "title",
+              widget: "string",
+            },
+            {
+              label: "Hero Description",
+              name: "description",
+              widget: "markdown",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+export default config;
---
+++ pages/admin.js
@@ -0,0 +1,17 @@
+import dynamic from "next/dynamic";
+import config from "../cms/config";
+
+const CMS = dynamic(
+  () =>
+    import("netlify-cms-app").then((cms) => {
+      cms.init({ config });
+    }),
+  // eslint-disable-next-line react/display-name
+  { ssr: false, loading: () => <p>Loading...</p> }
+);
+
+const AdminPage = () => {
+  return <CMS />;
+};
+
+export default AdminPage;
