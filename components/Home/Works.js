7b0fa98de786e05608bb705038dd39907fa44f56
Felipe Marcon <felipeemarcon@gmail.com>
2021-08-12 20:10:16 -0300

    start use swipe

--- components/Home/Works.js
+++ components/Home/Works.js
@@ -1,3 +1,6 @@
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
 // Site Components
 import Section from "@components/Section";
 import Container from "@components/Container";
@@ -7,6 +10,9 @@ import WorkItem from "@components/Home/WorkItem";
 
 // Styles
 import styles from "@styles/home/works.module.scss";
+import "swiper/swiper-bundle.css";
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 export default function Works({ works }) {
   return (
@@ -19,9 +25,22 @@ export default function Works({ works }) {
             </Heading>
           </div>
           <div className={styles.items}>
-            {works.map((work, index) => (
-              <WorkItem key={index} work={work} />
-            ))}
+            <Swiper
+              className={styles.swiper}
+              slidesPerView={3}
+              scrollbar={{ draggable: true }}
+              onSwiper={(swiper) => console.log(swiper)}
+              onSlideChange={() => console.log("slide change")}
+            >
+              {works.map((work, index) => (
+                <SwiperSlide key={index} className={styles.swipperSlide}>
+                  <WorkItem work={work} />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+            <div className="swiper-container">
+              <div className="swiper-wrapper"></div>
+            </div>
           </div>
         </Grid>
       </Container>
