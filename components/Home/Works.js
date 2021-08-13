1fabd8b4d3444f94e0ff65305eeb61c33e364803
Felipe Marcon <felipeemarcon@gmail.com>
2021-08-12 21:59:07 -0300

    add swiper component to works

--- components/Home/Works.js
+++ components/Home/Works.js
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -7,6 +8,7 @@ import Container from "@components/Container";
 import Grid from "@components/Grid";
 import Heading from "@components/Heading";
 import WorkItem from "@components/Home/WorkItem";
+import Icon from "@components/Icon";
 
 // Styles
 import styles from "@styles/home/works.module.scss";
@@ -15,6 +17,9 @@ import "swiper/swiper-bundle.css";
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 export default function Works({ works }) {
+  const navigationPrevRef = useRef(null);
+  const navigationNextRef = useRef(null);
+
   return (
     <Section customClass={styles.worksSection}>
       <Container>
@@ -23,25 +28,52 @@ export default function Works({ works }) {
             <Heading type="h3">
               Places I <span className={styles.title_highlight}>worked</span>
             </Heading>
-          </div>
-          <div className={styles.items}>
-            <Swiper
-              className={styles.swiper}
-              slidesPerView={3}
-              scrollbar={{ draggable: true }}
-              onSwiper={(swiper) => console.log(swiper)}
-              onSlideChange={() => console.log("slide change")}
-            >
-              {works.map((work, index) => (
-                <SwiperSlide key={index} className={styles.swipperSlide}>
-                  <WorkItem work={work} />
-                </SwiperSlide>
-              ))}
-            </Swiper>
-            <div className="swiper-container">
-              <div className="swiper-wrapper"></div>
+            <div className={styles.swiperNavigation}>
+              <div
+                ref={navigationPrevRef}
+                className={styles.swiperNavigationControl}
+              >
+                <Icon icon="arrowLeft" size="32" />
+              </div>
+              <div
+                ref={navigationNextRef}
+                className={styles.swiperNavigationControl}
+              >
+                <Icon icon="arrowRight" size="32" />
+              </div>
             </div>
           </div>
+          <Swiper
+            className={styles.swiper}
+            navigation={{
+              prevEl: navigationPrevRef.current,
+              nextEl: navigationNextRef.current,
+              disabledClass: styles.swiperNavigationDisabled,
+            }}
+            onInit={(swiper) => {
+              swiper.params.navigation.prevEl = navigationPrevRef.current;
+              swiper.params.navigation.nextEl = navigationNextRef.current;
+              swiper.navigation.update();
+            }}
+            breakpoints={{
+              320: {
+                slidesPerView: 1,
+                scrollbar: { draggable: true },
+              },
+              748: {
+                slidesPerView: 2,
+              },
+              1360: {
+                slidesPerView: 3,
+              },
+            }}
+          >
+            {works.map((work, index) => (
+              <SwiperSlide key={index} className={styles.swipperSlide}>
+                <WorkItem work={work} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </Grid>
       </Container>
     </Section>
