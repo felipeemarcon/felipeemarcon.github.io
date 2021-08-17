ad6adad9d3f4bb1692ed35601275697dfb0be6a0
Felipe Marcon <felipeemarcon@gmail.com>
2021-08-17 13:08:01 -0300

    add siteConfig file

---
+++ src/siteConfig.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  title: "Felipe Marcon",
+  keywords:
+    "design, product, experience, brazil, brazillian designer, experiência, ux, ui, user experience, experiência do usuário, user interface, interface de usuário, freelance, project, design projects",
+  social: {
+    twitter: "@felipeemarcon",
+  },
+};
